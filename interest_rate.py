d81911c521384d7550d00a7620c2d8e086ec0c01
leito100 <leito100@mail.chapman.edu>
2016-02-18 23:16:27 -0800

    1.6

--- interest_rate.py
+++ interest_rate.py
@@ -4,5 +4,9 @@ Created on Thu Feb 18 22:56:01 2016
 
 @author: Paloma
 """
+A = 1000
+P = 0.05
+N = 3
+money = A*( 1 + ((P/100)**N))
 
-money =1000 (1 + (0.05/100)**3)
+print "The amount will be", money, " dollars after 3 years."
